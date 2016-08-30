e8ee781592b8d792d6a590763847c3f14efafee7
kiwifly <chudongjie713@gmail.com>
2016-08-31 00:48:58 +0800

    add wedding page

---
+++ js/script.js
@@ -0,0 +1,116 @@
+﻿(function($){
+	"use strict"; // Start of use strict
+
+	$(document).ready(function() {
+		"use strict";
+
+		
+		/*ScrollR */
+		if ($(window).width() > 1280) {
+		var s = skrollr.init({
+			forceHeight: false
+		});
+		}
+
+		/*Gallery ColorBox */
+		$('.gallery_txt a').colorbox({
+			rel:'gal',
+			maxWidth:"100%",
+		});
+
+		/* Section Background */
+		$('section, .parallax').each(function(){
+			var image = $(this).attr('data-image');
+			if (image){
+				$(this).css('background-image', 'url('+image+')');	
+			}
+		});
+		
+		/*ColorBox*/
+		if ($(window).width() >= 760) {
+			$(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+		}else{
+			$(".youtube").colorbox({iframe:true, innerWidth:320, innerHeight:240});
+		}
+		$(window).resize(function () {
+			if ($(window).width() >= 760) {
+				$(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+			}else{
+				$(".youtube").colorbox({iframe:true, innerWidth:320, innerHeight:240});
+			}
+		 });
+		
+		/*Scroll Effect*/
+		$('.intro_down, .go').on("click", function(e){
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+		
+		/*Show/Hide Photo in When&Where Block*/
+		$('.photocamera span').on("click", function(e){
+			$(this).parents('section').find('.opacity').toggleClass('fade');
+			$(this).parents('section').find('.over').fadeToggle();
+			e.preventDefault();
+		});
+
+		/*Player*/
+		$('.music').on("click", function(e){
+			$('.player').fadeToggle();
+			e.preventDefault();
+		});
+
+		
+		/*CountDown*/
+		$('.married_coundown').countdown({until: new Date(2016, 8, 13)});
+		
+		/*OWL Carousel in Our Story*/
+		$(".story_wrapper").owlCarousel({
+	 		navigation : true,	responsive: true, responsiveRefreshRate : 200,	slideSpeed : 200,	
+	 		paginationSpeed : 200,	rewindSpeed : 500,	items:3,  itemsTablet: [768,1], autoPlay : true,
+			itemsMobile : [479,1], 	itemsDesktopSmall : [980,1],  itemsDesktop : [1500,2], mouseDrag:false, touchDrag:false
+		});
+
+		/*Gallery Carousel */
+		$(".gallery_wrapper").owlCarousel({
+	 		navigation : true,	responsive: true, responsiveRefreshRate : 200,	slideSpeed : 200,	
+	 		paginationSpeed : 200,	rewindSpeed : 500,	items:3,  itemsTablet: [768,2], autoPlay : true,
+			itemsMobile : [479,1], mouseDrag:false, touchDrag:false
+		});
+
+		/*Registry Carousel */
+		$(".registry_wrapper").owlCarousel({
+	 		navigation : true, responsive: true, responsiveRefreshRate : 200, slideSpeed : 200, paginationSpeed : 200,
+			rewindSpeed : 500,	stopOnHover : true, autoHeight : true, items:3, mouseDrag:false, autoPlay : true, touchDrag:false
+		});
+
+		/*The Crew Carousel*/
+		$(".guest_wrapper").owlCarousel({
+	 		navigation : true, responsive: true, responsiveRefreshRate : 200, slideSpeed : 200,	paginationSpeed : 200,
+			rewindSpeed : 500, stopOnHover : true, autoHeight : true, items:4, mouseDrag:false, autoPlay : true, touchDrag:false
+		});	
+
+		/*Slider Carousel*/
+		$(".slider").owlCarousel({
+	 		navigation : true, responsive: true, responsiveRefreshRate : 200, slideSpeed : 200,	paginationSpeed : 200,
+			rewindSpeed : 500, stopOnHover : false, autoHeight : true, singleItem:true, mouseDrag:false, autoPlay : true, transitionStyle : "fade", touchDrag:false
+		});	
+
+		/*Blog Inside*/
+		$(".blog_inside_wrapper").owlCarousel({
+	 		navigation : true, responsive: true, responsiveRefreshRate : 200, slideSpeed : 200,	paginationSpeed : 200,
+			rewindSpeed : 500, stopOnHover : false, autoHeight : true, singleItem:true, mouseDrag:false, autoPlay : true, transitionStyle : "fade", touchDrag:false
+		});
+		
+		/*FireFly in Intro*/
+		$.firefly({
+			color: '#fff', minPixel: 1, maxPixel: 3, total : 55, on: '.into_firefly'
+		});
+		
+		/* Refresh ScrollR */
+		s.refresh($(".guest_wrapper, .our_story"));
+
+	});
+})(jQuery);
